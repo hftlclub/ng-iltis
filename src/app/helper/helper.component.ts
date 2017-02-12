a7d46a91c9a0727c5f8368d08e992c2ef4a27b28
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-02-12 15:46:01 +0100

    First attempts with bootstrap

---
+++ src/app/helper/helper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'il-helper',
+  templateUrl: './helper.component.html',
+  styleUrls: ['./helper.component.css']
+})
+export class HelperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
