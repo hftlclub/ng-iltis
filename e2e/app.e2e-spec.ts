52c1e240091e7b018286e79088b3772471e66f39
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-02-17 17:50:06 +0100

    Updated to @angular/cli@1.0.0-beta.32.3

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { IltisPage } from './app.po';
 
-describe('iltis App', function() {
+describe('iltis App', () => {
   let page: IltisPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('iltis App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('il works!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class IltisPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('il-root h1')).getText();
   }
 }
