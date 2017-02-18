d3da56aad033d21ac940af20368a21a08ac7d10f
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-02-18 11:48:58 +0100

    product-jump-bar component

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -14,6 +14,7 @@ import { TransferFormComponent } from './transfer-form/transfer-form.component';
 import { CounterControlComponent } from './counter-control/counter-control.component';
 import { ProductService } from './shared/product.service';
 import { ProductGroupComponent } from './product-group/product-group.component';
+import { ProductJumpBarComponent } from './product-jump-bar/product-jump-bar.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,8 @@ import { ProductGroupComponent } from './product-group/product-group.component';
     ProductOverviewComponent,
     TransferFormComponent,
     CounterControlComponent,
-    ProductGroupComponent
+    ProductGroupComponent,
+    ProductJumpBarComponent
   ],
   providers: [
     ProductService
---
+++ src/app/event/product-jump-bar/product-jump-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'il-product-jump-bar',
+  templateUrl: './product-jump-bar.component.html',
+  styleUrls: ['./product-jump-bar.component.css']
+})
+export class ProductJumpBarComponent {
+
+  @Input() categories;
+
+}
