dd7170303626321c6d6f19760fe3857a78b2350c
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-02-28 01:09:46 +0100

    Updated valuechecker from node-iltis

--- src/app/shared/valuechecker.ts
+++ src/app/shared/valuechecker.ts
@@ -1,8 +1,13 @@
 export class ValueChecker {
 
     static validNumber(no: number) {
+        return typeof no == 'number';
+    }
+
+    static validBooleanNumber(no: number) {
         return no && typeof no == 'number';
     }
+
 
     static validString(str: string) {
         return str && typeof str == 'string';
