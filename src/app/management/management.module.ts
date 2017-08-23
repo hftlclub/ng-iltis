00829bad9e9ba973b7c4bc9c07258fddc701e6cf
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-08-23 20:45:28 +0200

    Mgmt Dashboard with big blue buttons

--- src/app/management/management.module.ts
+++ src/app/management/management.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ManagementRoutingModule } from './management-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ManagementRoutingModule
+    ManagementRoutingModule,
+    SharedModule
   ],
   declarations: [DashboardComponent]
 })
