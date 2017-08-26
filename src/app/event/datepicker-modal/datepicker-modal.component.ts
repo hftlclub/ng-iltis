f123815c72d54a90643221868c59f476f0f4efd4
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-08-26 14:37:11 +0200

    Datepicker preselection for next/last wednesday fixed #37

--- src/app/event/datepicker-modal/datepicker-modal.component.ts
+++ src/app/event/datepicker-modal/datepicker-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
+
 @Component({
   selector: 'il-datepicker-modal',
   templateUrl: './datepicker-modal.component.html',
@@ -16,8 +17,23 @@ export class DatepickerModalComponent {
   setDate(mode: string): void {
     switch (mode) {
       case 'yesterday': this.date = new Date(Date.now() - 86400000); break;
+      case 'lastwed': this.date = this.getNextLastWeekday(3, -1); break;
+      case 'nextwed': this.date = this.getNextLastWeekday(3, 1); break;
       default: this.date = new Date();
     }
+  }
+
+  getNextLastWeekday(d: number, direction: number = 1): Date {
+    // direction: 1 forward, -1 backwards
+    // d: wanted day 0...6
+    // m: current day 0...6
+    direction = direction > 0 ? 1 : -1;
+
+    const m = new Date().getUTCDay();
+    d = d % 7;
+    const dayOffset = (direction * (d - m) + 6) % 7 + 1;
+    // (-1 * (6 - 3) + 6) % 7 + 1
+    return new Date(Date.now() + (86400000 * direction * dayOffset));
   }
 
   submitForm() {
