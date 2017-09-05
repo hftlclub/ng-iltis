4b65625fc4de9df176723f978f8efdf7cad3d995
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-09-05 14:34:02 +0200

    Sizetype amount is not editable

--- src/app/management/sizes/size-type-form/size-type-form.component.ts
+++ src/app/management/sizes/size-type-form/size-type-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { SizeType, SizeTypeFactory } from '../../../shared/models/sizetype';
+import { HelperService } from '../../../core/helper.service';
 
 @Component({
   selector: 'il-size-type-form',
@@ -10,6 +11,7 @@ import { SizeType, SizeTypeFactory } from '../../../shared/models/sizetype';
 })
 export class SizeTypeFormComponent implements OnInit {
 
+  @Input() edit = false;
   @Input() initialValue = SizeTypeFactory.empty();
   @Input() buttonIcon = 'ok';
   @Input() buttonLabel = 'Übernehmen';
@@ -19,18 +21,26 @@ export class SizeTypeFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private hs: HelperService) { }
 
   ngOnInit() {
     this.form = this.fb.group({
       description: [this.initialValue.description, [Validators.required]],
-      amount: [this.initialValue.amount, [Validators.required]]
+      amount: [this.hs.dotToComma(this.initialValue.amount), [Validators.required]]
     });
+
+    if (this.edit) {
+      this.form.get('amount').disable();
+    }
   }
 
   submitForm() {
-    const sizeType = this.form.value as SizeType;
-    sizeType.amount = parseFloat(this.form.value.amount);
+    const formValue = this.form.value;
+    const sizeType = {
+      description: formValue.description,
+      amount: formValue.amount ? this.hs.commaToNumber(this.form.value.amount) : null
+    } as SizeType;
+
     this.formSubmitted.emit(sizeType);
   }
 
