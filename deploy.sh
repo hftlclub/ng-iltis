117766dcbcefd84b1c37198d996f670fc4f03fba
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-09-14 16:30:54 +0200

    Removed deploy script, added params to ng build:prod

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-git pull origin release
-npm install
-npm run ng -- build -prod --aot --no-vendor-chunk --extract-css
-rm -fr /srv/www/iltis/html
-mv dist /srv/www/iltis/html
