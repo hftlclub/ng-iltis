1b9b0aa84dca5abaa090a2129149af1a8fa5c0b4
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-11-28 12:35:04 +0100

    Collapsable navbar for smaller screens
    fix #86

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'il-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   aclass = 'active';
   collapsed = true;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(ev => ev instanceof NavigationEnd)
+    ).subscribe(ev => this.collapsed = true);
+  }
+
+  toggleCollapse() {
+    this.collapsed = !this.collapsed;
+  }
 }
