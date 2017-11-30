1f8dc22d72d34cd26e75aa046e60003f3d3977e3
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-11-30 13:38:08 +0100

    autoscroll history list
    resolve #29

--- src/app/event/history-sidebar/history-sidebar.component.ts
+++ src/app/event/history-sidebar/history-sidebar.component.ts
@@ -1,7 +1,9 @@
+import { PageScrollInstance, PageScrollService } from 'ngx-page-scroll';
 import { switchMap } from 'rxjs/operators';
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Inject, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
+import { DOCUMENT } from '@angular/common';
 
 import { Transfer } from '../../shared/models/transfer';
 import { Transaction } from '../../shared/models/transaction';
@@ -27,7 +29,16 @@ export class HistorySidebarComponent implements OnInit, OnDestroy {
   countFinishedSub: Subscription;
   eventClosedSub: Subscription;
 
-  constructor(private route: ActivatedRoute, private es: EventService) { }
+  scroller: PageScrollInstance;
+
+  @ViewChild('scrollArea') scrollArea: ElementRef;
+
+  constructor(
+    private route: ActivatedRoute,
+    private es: EventService,
+    private pageScrollService: PageScrollService,
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   ngOnInit() {
     this.event = this.route.snapshot.data['event'];
@@ -35,10 +46,16 @@ export class HistorySidebarComponent implements OnInit, OnDestroy {
     this.transactions = this.route.snapshot.data['transactions'];
 
     this.transfersAddedSub = this.es.transfersAdded
-      .subscribe(t => this.transfers = this.transfers.concat(t));
+      .subscribe(t => {
+        this.transfers = this.transfers.concat(t);
+        setTimeout(() => this.scrollToBottom(), 200);
+      });
 
     this.countFinishedSub = this.es.countFinished
-      .subscribe(t => this.transfers = t);
+      .subscribe(t => {
+        this.transfers = t;
+        setTimeout(() => this.scrollToBottom(), 200);
+      });
 
     this.eventClosedSub = this.es.eventClosed.pipe(
       switchMap(eventId => this.es.getTransactionsByEvent(eventId))
@@ -48,6 +65,21 @@ export class HistorySidebarComponent implements OnInit, OnDestroy {
       this.transactions = transactions;
     });
 
+    this.setupScroller();
+  }
+
+  setupScroller() {
+    this.scroller = PageScrollInstance.newInstance({
+      document: this.document,
+      scrollTarget: '#newTransferBtn',
+      scrollingViews: [this.scrollArea.nativeElement],
+      pageScrollDuration: 2000
+    });
+
+  }
+
+  scrollToBottom() {
+    this.pageScrollService.start(this.scroller);
   }
 
 
